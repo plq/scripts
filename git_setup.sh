78b1940374727baaf0827f565a9b5692229d52ba
Burak Arslan <burak.arslan@arskom.com.tr>
2012-05-15 18:39:30 +0300

    git setup geldi

---
+++ git_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git config --global user.name "Burak Arslan"
+git config --global user.email "burak.arslan@arskom.com.tr"
+git config --global core.pager ''
+git config --global color.ui true
