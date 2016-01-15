3e9e112ddfd4df4a095d943a11472b26a0390fac
Burak Arslan <burak.arslan@arskom.com.tr>
2016-01-15 12:26:47 +0200

    big_screen with nvidia support

--- big_screen.sh
+++ big_screen.sh
@@ -1,11 +1,19 @@
+if [ -n "$(xrandr | grep HDMI-1-0)" ]; then
+    HDMI=HDMI-1-0
+    LVDS=LVDS-1-0
+else
+    LVDS=LVDS1
+    HDMI=HDMI1
+fi
+
 xrandr --newmode $(cvt 2560 1440 30 | grep -v ^\# | cut -b 10-)
-xrandr --addmode HDMI1 $(cvt 2560 1440 30 | grep -v ^\# | cut -d " " -f 2)
-xrandr --output HDMI1 --mode $(cvt 2560 1440 30 | grep -v ^\# | cut -d " " -f 2)
+xrandr --addmode $HDMI $(cvt 2560 1440 30 | grep -v ^\# | cut -d " " -f 2)
+xrandr --output $HDMI --mode $(cvt 2560 1440 30 | grep -v ^\# | cut -d " " -f 2)
 
 if [ -z "$1" ]; then
     SCREEN_STATE="--off"
 else
-    SCREEN_STATE="$1 HDMI1"
+    SCREEN_STATE="$1 $HDMI"
 fi
 
-xrandr --output LVDS1 $SCREEN_STATE
+xrandr --output $LVDS $SCREEN_STATE
