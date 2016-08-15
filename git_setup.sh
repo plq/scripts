efe869731f225311c52eebe19d7ffe297f048f57
Burak Arslan <burak.arslan@arskom.com.tr>
2016-08-15 09:19:58 +0300

    git setup global ignores

--- git_setup.sh
+++ git_setup.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git config --global user.name "Burak Arslan"
 git config --global user.email "burak.arslan@arskom.com.tr"
 git config --global core.pager ''
 git config --global color.ui true
+git config --global core.excludesfile "$DIR/global_ignores"
