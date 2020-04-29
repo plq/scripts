d1968ebdc37db9c7fb176dda8d64d1e1e5a3dee6
Burak Arslan <burak.arslan@arskom.com.tr>
2020-04-29 18:38:37 +0300

    sess

--- kde-unlock.sh
+++ kde-unlock.sh
@@ -1,4 +1,7 @@
-#!/bin/sh -x
+#!/bin/bash -x
+ck-list-sessions
 
-SESS=$(ck-list-sessions | grep -B1000 "x11-display-device = '/dev/tty7'" | egrep -o ^Session[0-9]+ | tail -n1)
+[ -z "${SESS}" ] && \
+    SESS=$(ck-list-sessions | grep -B1000 "x11-display-device = '/dev/tty7'" | egrep -o ^Session[0-9]+ | tail -n1)
+
 dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/"${SESS}" org.freedesktop.ConsoleKit.Session.Unlock
