3e389f9aee1801b47db5f17c4dc006614fae2749
Burak Arslan <burak.arslan@arskom.com.tr>
2020-04-29 18:44:56 +0300

    fix mac ps1

--- fun_stuff.sh
+++ fun_stuff.sh
@@ -8,7 +8,7 @@ function parse_git_branch {
    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
 }
 if [ "$(uname)" == "Darwin" ]; then
-    export PS1='\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\]'
+    export PS1='\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
 fi
 
 export PS1="$PS1\$(parse_git_branch)"
